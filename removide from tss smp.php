9d1752616afba5716ec92998496a3be2b388f2ef
Subbrat <m.subbrat@outlook.com>
2023-02-26 19:27:39 +0530

    as

---
+++ removide from tss smp.php
@@ -0,0 +1,60 @@
+<section class="s-hide-large  course__section d-none d-lg-flex mx-5 border rounded-1 p-5">
+    <div class="d-flex align-items-start w-100">
+        <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+            <h5 class="course__left__side__heading mb-lg-4">
+                Colleges
+            </h5>
+            <div class="nav-pills--inner--container">
+                <button class="nav-link course__left__side__li__a mb-4 px-5 py-3 btn active" id="13" data-bs-toggle="pill" data-bs-target="#pills-13" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">TSS NISER</button>
+            </div>
+        </div>
+        <div class="tab-content w-100" id="v-pills-tabContent">
+            <div class="">
+                <div class="row mb-3">
+                    <div class="col-md-4">
+                        <h5 class="course__left__side__heading mb-lg-4 ps-3">
+                            Courses
+                        </h5>
+                    </div>
+                    <div class="col-md-4 ms-auto">
+                        <form action="search.php" method="POST">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Polymers, Biomolecules, Chemistry in everyday life, etc..." value="" name="searchBar" id="searchBar" required="">
+                                <button class="btn border" type="submit" value="Search"><i class="fa-solid fa-magnifying-glass"></i></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane fade showive row" id="pills-13" role="tabpanel" aria-labelledby="pills-13" tabindex="13">
+                <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 mb-4" style="width:25rem;">
+                    <div class="p-4 rounded-3 course__right__side__card">
+                        <h1 class="course__main__heading fs-3 mb-3">
+                            Class 6
+                        </h1>
+                        <a href="sciastra.com" class="text-black-50 fw-semibold ">View Details
+                            &nbsp;
+                            <i class="bi bi-arrow-right"></i>
+                        </a>
+                        <div class="d-flex justify-content-between mt-4 align-items-center">
+                            <div class="position-relative">
+                                <p class="course__section__price mb-0 old__price">
+                                    ₹&nbsp;100
+                                </p>
+                                <p class="course__section__price mb-0 new__price ">
+                                    ₹&nbsp;10
+                                </p>
+                                <p class="course__discount__batch">90% OFF</p>
+                            </div>
+                            <a href="sciastra.com" class="course__buy__btn">
+                                <span>
+                                    Buy Now
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
