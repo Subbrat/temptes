13c4ea1348bf1d052038d4820033dd7bb6f9c964
Subbrat <m.subbrat@outlook.com>
2024-07-07 20:33:50 +0530

    asda

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/jquery.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/dataTables.bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/buttons.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Beautiful Tables</title>
+</head>
+
+<body class="container">
+    <h1 class="text-center">Sample DataTables Project</h1>
+    <table id="maintable" class="display compact cell-border" cellspacing="0" width="100%">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>City</th>
+                <th>Age</th>
+                <th>Start Date</th>
+                <th>Salary</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger Nixon</td>
+                <td>System Architecture</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>2011/04/25</td>
+                <td>$320,800</td>
+            </tr>
+            <tr>
+                <td> Harold Franklin</td>
+                <td> Financial Advisor</td>
+                <td> Tuwiri Wetan</td>
+                <td> 43</td>
+                <td> 06/18/2011</td>
+                <td> $159671.33</td>
+            </tr>
+            <tr>
+                <td> Rose Porter</td>
+                <td> Geologist IV</td>
+                <td> Banjar Sandinggianyar</td>
+                <td> 36</td>
+                <td> 12/11/2010</td>
+                <td> $204836.19</td>
+            </tr>
+            <tr>
+                <td> Emily Medina</td>
+                <td> Executive Secretary</td>
+                <td> Schaumburg</td>
+                <td> 37</td>
+                <td> 08/11/2010</td>
+                <td> $290542.71</td>
+            </tr>
+        </tbody>
+        <tfoot style="background-color: #c0c0c0; color: #ffffff; font-size: 0.9em; ">
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>City</th>
+                <th>Age</th>
+                <th>Start Date</th>
+                <th>Salary</th>
+            </tr>
+        </tfoot>
+    </table>
+    <script type="text/javascript" src="js/jquery-2.2.4.min.js"></script>
+    <script type="text/javascript" src="js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" src="js/jszip.min.js"></script>
+    <script type="text/javascript" src="js/pdfmake.min.js"></script>
+    <script type="text/javascript" src="js/vfs_fonts.js"></script>
+    <script type="text/javascript" src="js/buttons.html5.min.js"></script>
+    <script type="text/javascript" src="js/buttons.print.min.js"></script>
+    <script type="text/javascript" src="js/app.js"></script>
+    <script type="text/javascript" src="js/jquery.mark.min.js"></script>
+    <script type="text/javascript" src="js/datatables.mark.js"></script>
+    <script type="text/javascript" src="js/buttons.colVis.min.js"></script>
+    <footer>For documentation and licensing visit <a href="https://datatables.net/">DataTables</a></footer>
+</body>
+
+</html>
